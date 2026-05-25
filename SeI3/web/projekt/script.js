cb49ec1581fb7bb04b96eb332d3c7cbbaac51634
Kraina Jindřich <kraina.jindrich@gkh.cz>
2026-05-25 09:41:59 +0200

    syncing changes

--- SeI3/web/projekt/script.js
+++ SeI3/web/projekt/script.js
@@ -3,3 +3,9 @@ document.addEventListener("scroll", (e) => {
   if (!nav) nav = document.getElementsByTagName("nav")[0];
   nav.toggleAttribute("data-active", window.scrollY > 50);
 });
+
+const weekDays = document.getElementById("week-days");
+const date = new Date();
+[...weekDays.children].forEach((day, idx) => {
+  day.toggleAttribute("data-active", date.getDay() == idx + 1);
+});
