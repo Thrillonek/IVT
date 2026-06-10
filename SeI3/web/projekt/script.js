df9b4d9e612c6a9b309145639d9c1f56a0567745
Thrillonek <jindrich.kraina@gmail.com>
2026-06-10 06:38:35 +0200

    Navbar and hero change

--- SeI3/web/projekt/script.js
+++ SeI3/web/projekt/script.js
@@ -1,11 +1,11 @@
 let nav;
-document.addEventListener("scroll", (e) => {
-  if (!nav) nav = document.getElementsByTagName("nav")[0];
-  nav.toggleAttribute("data-active", window.scrollY > 50);
+document.addEventListener('scroll', (e) => {
+	if (!nav) nav = document.getElementsByTagName('nav')[0];
+	nav.toggleAttribute('data-active', window.scrollY > 50);
 });
 
-const weekDays = document.getElementById("week-days");
+const weekDays = document.getElementById('week-days');
 const date = new Date();
 [...weekDays.children].forEach((day, idx) => {
-  day.toggleAttribute("data-active", date.getDay() == idx + 1);
+	day.toggleAttribute('data-fixed', date.getDay() == idx + 1);
 });
