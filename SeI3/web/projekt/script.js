03b09e521732eeb60ccbda2de6988b6db7837b3c
Thrillonek <jindrich.kraina@gmail.com>
2026-06-10 18:08:51 +0200

    Added icon, made it responsive

--- SeI3/web/projekt/script.js
+++ SeI3/web/projekt/script.js
@@ -1,11 +1,16 @@
 let nav;
 document.addEventListener('scroll', (e) => {
 	if (!nav) nav = document.getElementsByTagName('nav')[0];
-	nav.toggleAttribute('data-active', window.scrollY > 50);
+	nav.toggleAttribute('data-fixed', window.scrollY > 50);
 });
 
 const weekDays = document.getElementById('week-days');
 const date = new Date();
 [...weekDays.children].forEach((day, idx) => {
-	day.toggleAttribute('data-fixed', date.getDay() == idx + 1);
+	day.toggleAttribute('data-active', date.getDay() == idx + 1);
 });
+
+function toggleNavbar() {
+	const navbar = document.getElementsByTagName('nav')[0];
+	navbar.toggleAttribute('data-visible');
+}
